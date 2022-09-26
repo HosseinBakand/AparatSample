73bac15ed395471e6648027faaf8ee089ce3db34
hossein <hosein.bakand@partdp.ir>
2022-09-26 09:24:23 +0330

    migrate to kotlin dsl(#7) - update gradle

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.41")
+    }
+}
+
+
+tasks.register("clean",Delete::class ){
+    delete(rootProject.buildDir)
+}
